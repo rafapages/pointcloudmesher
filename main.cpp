b3e1cfff082d5451d5d2903ad0a5ff8e55663299
rps <rps@gti.ssr.upm.es>
2015-10-14 15:15:07 +0200

    program_option now works for two different cases

--- main.cpp
+++ main.cpp
@@ -6,7 +6,11 @@
 #include <boost/make_shared.hpp>
 #include <boost/program_options.hpp>
 
+typedef enum {ALL, POISSON} RunMode;
+
 int main (int argc, char *argv[]){
+
+    RunMode mode = ALL;
 
     // Parsing execution options
     try {
@@ -34,9 +38,10 @@ int main (int argc, char *argv[]){
 
         if (vm.count("poisson")) {
             std::cerr << "Only poisson reconstruction will now run." << std::endl;
-
+            mode = POISSON;
         } else {
             std::cerr << "Complete system will now run" << std::endl;
+            mode = ALL;
         }
 
     }
@@ -52,112 +57,113 @@ int main (int argc, char *argv[]){
     }
 
 
-
-
-    if (argc != 3){
-        std::cerr << "Wrong number of input paremeters" << std::endl;
-        std::cerr << "Usage: " << argv[0] << " <bundlerfile.out> <image-list.txt>" << std::endl;
-        return 0;
-    }
-
-    std::string namein(argv[1]);
-    std::string nameout = namein.substr(0, namein.size()-4);
-
     PcMesher cloud;
 
-    // Reading input parameteres: blunder file and image list
-    cloud.bundlerReader(argv[1]);
-    cloud.readImageList(argv[2]);
-    cloud.writeCameraSetupFile(nameout + "_cameras.txt");
-//    cloud.readCloud(argv[1]);
 
-    cloud.writeCloud(nameout + "_input.ply");
+    // MODE = ALL
 
-    // Statistical outlier removal
-    cloud.removeAllOutliers();
+    if (mode == ALL){
 
-    // We first estimate normals to get an initial orientation
-    cloud.estimateAllNormals(0.1);
-    cloud.writeCloud(nameout + "_sinoutliers.ply");
+        if ((argc != 3) && (argc != 4)){
+            std::cerr << "Wrong number of input paremeters for complete mode" << std::endl;
+            std::cerr << "Usage: " << argv[0] << " [-a] <bundlerfile.out> <image-list.txt>" << std::endl;
+            return 1;
+        }
 
-    // Planes are segmented also using their normal info
-    cloud.segmentPlanes();
-    // Normals are estimated properly now (and their sense corrected)
-    cloud.estimateAllNormals(0.3);
-    cloud.fixAllNormals();
 
-    // "Planar" point clouds are collected into a single point cloud
-    std::vector<PointCloud<PointXYZRGBNormalCam>::Ptr> pointclouds;
-    for (unsigned int i = 1; i < cloud.getNClouds(); i++){
-        pointclouds.push_back(cloud.getPointCloudPtr(i));
+        int index = 1;
+        if (argc == 4){
+            index = 2;
+        }
+
+        std::string namein(argv[index]);
+        std::string listname(argv[index+1]);
+
+
+        std::string nameout = namein.substr(0, namein.size()-4);
+
+
+        // Reading input parameteres: blunder file and image list
+        cloud.bundlerReader(namein);
+        cloud.readImageList(listname);
+        cloud.writeCameraSetupFile(nameout + "_cameras.txt");
+        //    cloud.readCloud(argv[1]);
+
+        cloud.writeCloud(nameout + "_input.ply");
+
+        // Statistical outlier removal
+        cloud.removeAllOutliers();
+
+        // We first estimate normals to get an initial orientation
+        cloud.estimateAllNormals(0.1);
+        cloud.writeCloud(nameout + "_sinoutliers.ply");
+
+        // Planes are segmented also using their normal info
+        cloud.segmentPlanes();
+        // Normals are estimated properly now (and their sense corrected)
+        cloud.estimateAllNormals(0.3);
+        cloud.fixAllNormals();
+
+        // "Planar" point clouds are collected into a single point cloud
+        std::vector<PointCloud<PointXYZRGBNormalCam>::Ptr> pointclouds;
+        for (unsigned int i = 1; i < cloud.getNClouds(); i++){
+            pointclouds.push_back(cloud.getPointCloudPtr(i));
+        }
+
+        PointCloud<PointXYZRGBNormalCam> combinedCloud = cloud.combinePointClouds(pointclouds);
+        PointCloud<PointXYZRGBNormalCam>::Ptr combinedCloudPtr = boost::make_shared<PointCloud<PointXYZRGBNormalCam> >(combinedCloud);
+        io::savePLYFile(nameout + "_combined_planes.ply", combinedCloud);
+
+
+        PolygonMesh first_mesh = cloud.surfaceReconstruction(combinedCloudPtr);
+        io::savePLYFile(nameout + "_poisson.ply", first_mesh);
+
+        PolygonMesh m = cloud.deleteWrongVertices(combinedCloudPtr, first_mesh);
+
+        cloud.assignCam2Mesh(m, combinedCloudPtr, nameout + "_meshcamera.txt");
+        PolygonMesh ms = cloud.smoothMeshLaplacian(m);
+        PolygonMesh simpleM = cloud.decimateMesh(ms);
+
+        io::savePLYFile(nameout + "_poisson_limpio.ply", m);
+        io::savePLYFile(nameout + "_poisson_limpio_smooth.ply", ms);
+        io::savePLYFile(nameout + "_poisson_limpio_smooth_decimated.ply", simpleM);
+
+        cloud.assignCam2Mesh(ms, combinedCloudPtr, nameout + "_meshcamera.txt");
+        cloud.assignCam2Mesh(simpleM, combinedCloudPtr, nameout + "_limpio_decimated_meshcamera.txt");
+
+        //    cloud.drawCameras();
+
+        cloud.writeCloud(nameout + "_output.ply");
     }
 
-    PointCloud<PointXYZRGBNormalCam> combinedCloud = cloud.combinePointClouds(pointclouds);
-    PointCloud<PointXYZRGBNormalCam>::Ptr combinedCloudPtr = boost::make_shared<PointCloud<PointXYZRGBNormalCam> >(combinedCloud);
-    io::savePLYFile(nameout + "_combined_planes.ply", combinedCloud);
 
+    //------------------------------------------------------------------------
+    // Just Poisson
+    //------------------------------------------------------------------------
 
-    PolygonMesh first_mesh = cloud.surfaceReconstruction(combinedCloudPtr);
-    io::savePLYFile(nameout + "_poisson.ply", first_mesh);
+    if (mode == POISSON){
 
-    PolygonMesh m = cloud.deleteWrongVertices(combinedCloudPtr, first_mesh);
+        if (argc != 3){
+            std::cerr << "Wrong number of input paremeters" << std::endl;
+            std::cerr << "Usage: " << argv[0] << " -p <pointCloud.ply>" << std::endl;
+            return 1;
+        }
 
-    cloud.assignCam2Mesh(m, combinedCloudPtr, nameout + "_meshcamera.txt");
-    PolygonMesh ms = cloud.smoothMeshLaplacian(m);
-    PolygonMesh simpleM = cloud.decimateMesh(ms);
+        // Reading input parameteres: blunder file and image list
+        cloud.readPLYCloud(argv[2]);
 
-    io::savePLYFile(nameout + "_poisson_limpio.ply", m);
-    io::savePLYFile(nameout + "_poisson_limpio_smooth.ply", ms);
-    io::savePLYFile(nameout + "_poisson_limpio_smooth_decimated.ply", simpleM);
+        PolygonMesh first_mesh = cloud.surfaceReconstruction(cloud.getPointCloudPtr(0));
+        io::savePLYFile("poisson_v2.ply", first_mesh);
 
-    cloud.assignCam2Mesh(ms, combinedCloudPtr, nameout + "_meshcamera.txt");
-    cloud.assignCam2Mesh(simpleM, combinedCloudPtr, nameout + "_limpio_decimated_meshcamera.txt");
+        PolygonMesh m = cloud.deleteWrongVertices(cloud.getPointCloudPtr(0), first_mesh);
+        PolygonMesh ms = cloud.smoothMeshLaplacian(m);
 
-//    cloud.drawCameras();
+        io::savePLYFile("poisson_clean_smooth.ply", ms);
+    }
 
-    cloud.writeCloud(nameout + "_output.ply");
 
     return 0;
 
-//    //------------------------------------------------------------------------
-//    // Just Poisson
-//    //------------------------------------------------------------------------
-
-//    if (argc != 2){
-//        std::cerr << "Wrong number of input paremeters" << std::endl;
-//        return 0;
-//    }
-
-//    PcMesher cloud;
-
-//    // Reading input parameteres: blunder file and image list
-//    cloud.readPLYCloud(argv[1]);
-
-//    PolygonMesh first_mesh = cloud.surfaceReconstruction(cloud.getPointCloudPtr(0));
-//    io::savePLYFile("poisson_v2.ply", first_mesh);
-
-//    PolygonMesh m = cloud.deleteWrongVertices(cloud.getPointCloudPtr(0), first_mesh);
-
-//    io::savePLYFile("poisson_limpio_v2.ply", m);
-
-//    return 0;
-
-//    //------------------------------------------------------------------------
-//    // Just Poisson smooth
-//    //------------------------------------------------------------------------
-//    if (argc != 2){
-//        std::cerr << "Wrong number of input paremeters" << std::endl;
-//        return 0;
-//    }
-
-//    PcMesher cloud;
-//    PolygonMesh mesh;
-//    cloud.readPLYMesh(argv[1], mesh);
-//    PolygonMesh smoothMes = cloud.smoothMeshLaplacian(mesh);
-
-//    io::savePLYFile("smooth.ply", smoothMes);
-
-//    return 0;
 
 
     //----------------------------------------------------------------------------
